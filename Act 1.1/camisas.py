bf7a51593357e784f9ca475548bad11bd81f1ab7
Gamaliel-Marines <gamaliel_projects@hotmail.com>
2023-08-16 09:36:34 -0600

    feat: camisas

--- Act 1.1/camisas.py
+++ Act 1.1/camisas.py
@@ -1,16 +1,23 @@
 import random
 
-TOTAL_CAMISAS = 30000
+TOTAL_CAMISAS = 3000
 
-
-def diasProduccion():
-    maquina1 = random.randint(200,1000)
-    maquina2 = random.randint(200,1000)
+def totalCamisas():
+    maquina1 = random.randint(200, 1000)
+    maquina2 = random.randint(200, 1000)
     suma = maquina1 + maquina2
     dias = 0
-    while TOTAL_CAMISAS < suma:
-        dias+=1
+    while TOTAL_CAMISAS > suma:
+        maquina1 = random.randint(200, 1000)
+        maquina2 = random.randint(200, 1000)
+        suma =  suma + maquina1 + maquina2
+        dias += 1
+        print("La cantidad de camisas producidas en el día", dias, "fueron:", suma)
+
+    print("La cantidad de días que se tardaron en producir las", TOTAL_CAMISAS, "camisas fueron:", dias)
+
+totalCamisas()
+
     
-    print(dias)
-    
-diasProduccion()
+if __name__ == "__main__":
+    totalCamisas()
