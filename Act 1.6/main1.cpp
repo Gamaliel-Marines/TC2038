fcb5ca72bbd6559c954123064c58f45455b4de8a
Carlos <A01708634@tec.mx>
2023-09-10 20:36:51 -0600

    Documentation for functions and calculation of complexity

--- Act 1.6/main1.cpp
+++ Act 1.6/main1.cpp
@@ -8,10 +8,30 @@ const int N = 5;
 const int M = 5;  
 
 
+/**
+ * @brief Checks if a cell is a valid move within the labyrinth. 
+ * 
+ * @param x 
+ * @param y 
+ * @param labyrinth 
+ * @return true 
+ * @return false 
+ * @complejidad O(1)
+ */
 bool isSafe(int x, int y, vector<vector<int>>& labyrinth) {
     return (x >= 0 && x < N && y >= 0 && y < M && labyrinth[x][y] == 1);
 }
 
+/**
+ * @brief Solves the labyrinth using the backtracking algorithm.
+ * 
+ * @param labyrinth 
+ * @param x 
+ * @param y 
+ * @return true 
+ * @return false 
+ * @complexity O(4^(N*M))
+ */
 bool solveLabyrinthBacktracking(vector<vector<int>>& labyrinth, int x, int y) {
     
     if (x == N - 1 && y == M - 1) {
@@ -35,6 +55,12 @@ bool solveLabyrinthBacktracking(vector<vector<int>>& labyrinth, int x, int y) {
     return false;
 }
 
+/**
+ * @brief Sets unvisited cells to zero in the labyrinth. 
+ * 
+ * @param labyrinth 
+ * @complexity O(N*M)
+ */
 void updateUnvisitedCells(vector<vector<int>>& labyrinth) {
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < M; j++) {
@@ -45,7 +71,12 @@ void updateUnvisitedCells(vector<vector<int>>& labyrinth) {
     }
 }
 
-
+/**
+ * @brief Prints the labyrinth.
+ * 
+ * @param labyrinth 
+ * @complexity O(N*M)
+ */
 void printLabyrinth(const vector<vector<int>>& labyrinth) {
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < M; j++) {
@@ -54,7 +85,12 @@ void printLabyrinth(const vector<vector<int>>& labyrinth) {
         cout << endl;
     }
 }
-
+/**
+ * @brief Main Function
+ * 
+ * @return int 
+ * @complexity O(4^(N*M)) 
+ */
 int main() {
     vector<vector<int>> labyrinth = {
         {1, 0, 1, 1, 1},
